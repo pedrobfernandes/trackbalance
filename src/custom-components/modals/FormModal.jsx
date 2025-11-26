807c6951c9b162be84edaf427918847e2757b47c
Pedro Fernandes <pedrobfernandes37@gmail.com>
2025-11-25 21:57:13 -0300

    teste(a11y):revertendo alteração de timeout

--- src/custom-components/modals/FormModal.jsx
+++ src/custom-components/modals/FormModal.jsx
@@ -146,7 +146,7 @@ export default function FormModal(props)
         {
             firstFocusableRef.current.focus();
         
-        }, 500);
+        }, 200);
         
 
         modalRef.current.addEventListener("keydown", handleKeyDown);
